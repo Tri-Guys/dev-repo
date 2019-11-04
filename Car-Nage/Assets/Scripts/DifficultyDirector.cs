276c80e7465393407dc49c40b84c0851ef1a950a
Thomas <55106438+B00358599@users.noreply.github.com>
2019-11-04 23:57:24 +0000

    Finally figured out how to upload folders

---
+++ Car-Nage/Assets/Scripts/DifficultyDirector.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DifficultyDirector : MonoBehaviour
+{
+
+    int difficultyLevel;
+    Time difficultyTimer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Car-Nage/Assets/Scripts/WeaponSystem.cs
@@ -0,0 +1,48 @@
+﻿/* using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSystem : MonoBehaviour
+{
+    private int gunRoll = 0;
+    private int ammoCount = 0;
+    GameObject cannonRadius cannonRadius;
+    GameObject gunCannon gunCannon;
+    GameObject gunScatter;
+    GameObject gunHydra;
+
+
+if (gunRoll == 1 && ammoCount > 0){
+Handles.color = Color.red;
+        Handles.DrawWireDisc(cubeExample.transform.position, new Vector3(0, 1, 0), cubeExample.circleSize);
+     gunCannon.SetActive(true);}
+
+if (ammoCount == 0){
+gunRoll = 0;
+ gunCannon.SetActive(false);}
+
+if (gunRoll = 2 && ammoCount > 0){
+    gunScatter.SetActive(true);
+
+if (ammoCount == 0){
+gunRoll = 0;
+ gunScatter.SetActive(false);
+
+ if(gunRoll == 3 && ammoCount > 0){
+    gunHydra.SetActive(true);}
+else{
+if (ammoCount == 0){
+gunRoll = 0;
+ gunHydra.SetActive(false);}
+    void Start()
+{
+
+}
+
+// Update is called once per frame
+void Update()
+{
+
+}
+}
+ */
